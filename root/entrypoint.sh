746890e7efa599274794de5672bb1c3442efdf29
sevenrats <sevenrats@tutanota.com>
2023-01-07 20:29:17 -0500

    healthcheck proof of concept

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -17,6 +17,49 @@ do
   fi
 done
 
+# Define Healthchecks
+
+_health_namecoind () {
+    local interval=15
+    local delay=30
+    local limit=600
+    while true;
+    do
+        sleep $interval
+
+        block_status=$(namecoin-cli getblockchaininfo)
+        net_status=$(namecoin-cli getnetworkinfo)
+        connections=$(echo  "$net_status" | grep '"connections":' | grep -o '[0-9]\+')
+
+        if [ $limit -gt 0 ]; then
+            limit=$(($limit - $interval))
+            if [ $connections -eq 0 ]; then
+                echo "Waiting for namecoind to connect."
+            fi
+        else
+            if [ $connections -eq 0 ]; then
+                echo "You have failed to connect to the namecoin network.  Add peers manually."
+                break
+            fi
+
+        if echo $block_status | grep -q '"initialblockdownload": true'; then
+            echo "Namecoin-Core is still syncing with the blockchain."
+            # TODO: print the blocks remaining
+        fi
+
+        if [ $delay -gt 0 ]; then
+            delay=$(($delay - $interval))
+        else
+            if [ $connections -gt 0 ]; then
+                echo "The Namecoin-Core HealthCheck is running! You are connected."
+            else
+                echo "The Namecoin-Core HealthCheck is running, but you are NOT connected."
+            fi
+        fi
+    done
+}
+
 # Run application
-namecoind -conf=$NMCCORE_CONF & \
+namecoind -conf=/data/$NMCCORE_CONF & \
+_health_namecoind & \
 wait -n
